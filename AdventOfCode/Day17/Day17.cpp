f1cb39a0c1091271b99e82ca0d80887d5c816b04
Isaac 'Atridas' Serrano Guasch <atridas87@gmail.com>
2020-12-12 07:12:38 +0000

    Project creation

---
+++ AdventOfCode/Day17/Day17.cpp
@@ -0,0 +1,21 @@
+#include <cassert>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+	std::ifstream infile("input.txt");
+
+	std::string line;
+	if (infile.is_open())
+	{
+		while (std::getline(infile, line))
+		{
+		}
+	}
+
+	std::cout << "Hello World!" << std::endl;
+}
